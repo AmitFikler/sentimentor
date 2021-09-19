482a701ee5fb59a12627be50289d568860960aed
AmitFikler <amitfikler@gmail.com>
2021-09-19 13:49:37 +0300

    Enables a function that requests access to the API according to the instructions

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
-const textarea = document.getElementById("textarea")
-const submitBtn = document.getElementById("submit-btn")
-const resultsDiv = document.getElementById("results")
-const action = document.getElementById("action")
+const textarea = document.getElementById("textarea");
+const submitBtn = document.getElementById("submit-btn");
+const resultsDiv = document.getElementById("results");
+const action = document.getElementById("action");
+
+
+async function getSentim() {
+    const response = await fetch("https://sentim-api.herokuapp.com/api/v1/", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        body: 
+            JSON.stringify({text: textarea.value})
+    });
+    const result = await response.json();
+    console.log(result);
+}
+
+submitBtn.addEventListener("click",getSentim);
