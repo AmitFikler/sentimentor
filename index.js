81535dcb5fa9836fd1c6f4798b865c83f4201f27
AmitFikler <amitfikler@gmail.com>
2021-09-19 13:45:41 +0300

    Defining variables that I might work with

---
+++ index.js
@@ -0,0 +1,4 @@
+const textarea = document.getElementById("textarea")
+const submitBtn = document.getElementById("submit-btn")
+const resultsDiv = document.getElementById("results")
+const action = document.getElementById("action")
